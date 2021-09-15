2dde4decca813ce0a4be553097c3483aa79093c6
Master Yi <xh765155143@gmail.com>
2021-09-15 10:35:14 +0800

    leetcode: 162

--- 162.寻找峰值.js
+++ 162.寻找峰值.js
@@ -1,26 +1,22 @@
 /*
- * @Author: xuhuan
- * @Date: 2019-08-27 23:11:02
- * @LastEditors: xuhuan
- * @LastEditTime: 2019-08-27 23:19:46
- * @Description: 
+ * @lc app=leetcode.cn id=162 lang=javascript
+ *
+ * [162] 寻找峰值
  */
+
+// @lc code=start
 /**
  * @param {number[]} nums
  * @return {number}
  */
-var findPeakElement = function(nums) {
-
+var findPeakElement = function (nums) {
   const length = nums.length;
   nums.push(-Infinity);
-  
+
   for (let index = 0; index < length; index++) {
     if (nums[index] > nums[index + 1]) {
       return index;
     }
   }
 };
-
-console.log(findPeakElement([1,2,3,4]));
-
-
+// @lc code=end
